eb9d3e49bd8a35800d2351c0d1c5ff5c5edaa778
navishd <navishmitha.gunawardana2@gmail.com>
2025-09-13 23:12:28 +0530

    entity

--- movieManagement/src/main/java/com/NxP/movieManagement/MovieManagementApplication.java
+++ movieManagement/src/main/java/com/NxP/movieManagement/MovieManagementApplication.java
@@ -1,4 +1,4 @@
-package com.NxP.movieManagement;
+package com.nxp.movieManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
